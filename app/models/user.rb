c7c55d6fe040df23b72ea91f159e8a6fd67fe966
kamikazenohiro <kamikazenohiro632@icloud.com>
2021-01-08 15:26:56 +0900

    最低限のバリデーションとストロングパラメーター

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname,           presence: true
+       # validates :encrypted_password, 半角英数字混合での入力が必須であること
+         validates :last_name,          presence: true #, 全角（漢字・ひらがな・カタカナ）での入力が必須であること
+         validates :first_name,         presence: true #, 全角（漢字・ひらがな・カタカナ）での入力が必須であること
+         validates :last_kana,          presence: true #, 全角（カタカナ）での入力が必須であること
+         validates :first_kana,         presence: true #, 全角（カタカナ）での入力が必須であること
+         validates :date,               presence: true
 end
