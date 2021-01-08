763c7b5b2a90d3c74e4cb2375c6fbe92fdd22235
kamikazenohiro <kamikazenohiro632@icloud.com>
2021-01-08 20:36:15 +0900

    新規登録のバリデーション設定やネームpasswordへの変更、ログイン画面設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname,           presence: true
-       # validates :encrypted_password, 半角英数字混合での入力が必須であること
-         validates :last_name,          presence: true #, 全角（漢字・ひらがな・カタカナ）での入力が必須であること
-         validates :first_name,         presence: true #, 全角（漢字・ひらがな・カタカナ）での入力が必須であること
-         validates :last_kana,          presence: true #, 全角（カタカナ）での入力が必須であること
-         validates :first_kana,         presence: true #, 全角（カタカナ）での入力が必須であること
+      
+      with_options presence: true do
+        validates :password,           format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters." }
+        validates :last_name,          format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters." }
+        validates :first_name,         format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters." }
+        validates :last_kana,          format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." }
+        validates :first_kana,         format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." }
+      end
+        # validates :last_name,          presence: true #, 全角（漢字・ひらがな・カタカナ）での入力が必須であること
+        # validates :first_name,         presence: true #, 全角（漢字・ひらがな・カタカナ）での入力が必須であること
+        # validates :last_kana,          presence: true #, 全角（カタカナ）での入力が必須であること
+        # validates :first_kana,         presence: true #, 全角（カタカナ）での入力が必須であること
          validates :date,               presence: true
 end
