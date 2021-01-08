9ac66b72567e473cc86129d43b1b89a12f30f095
kamikazenohiro <kamikazenohiro632@icloud.com>
2021-01-08 15:52:59 +0900

    deviseにストロングパラメーターを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_kana, :first_kana, :date])
   end
 end
