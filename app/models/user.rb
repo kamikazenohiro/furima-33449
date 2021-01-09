a294c605e5db1a461e3c54ff352101d816cf0210
kamikazenohiro <kamikazenohiro632@icloud.com>
2021-01-09 15:29:36 +0900

    パスワードのバリデーションとRspecのuserモデル追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname,           presence: true
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
       
       with_options presence: true do
-        validates :password,           format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters." }
         validates :last_name,          format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters." }
         validates :first_name,         format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters." }
         validates :last_kana,          format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." }
