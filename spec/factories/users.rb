fab25df81bea50d68ec5573f83cdcd9c58540a9f
kamikazenohiro <kamikazenohiro632@icloud.com>
2021-01-10 20:05:23 +0900

    Rubocopと単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              { 'test' }
+    email                 { 'test@example' }
+    password              { '0000sa' }
+    password_confirmation { password }
+    last_name             { 'てすと' }
+    first_name            { 'たろう' }
+    last_kana             { 'テスト' }
+    first_kana            { 'タロウ' }
+    date                  { '2000-01-01' }
+  end
+end
