6822a09c4c1dae9b094183a7139b49702a666bf5
kamikazenohiro <kamikazenohiro632@icloud.com>
2021-01-11 21:29:59 +0900

    delivery_dayのプルダウン実装とバリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,8 +8,10 @@ class Item < ApplicationRecord
   belongs_to :condition
   belongs_to :shipping_fee
   belongs_to :prefecture
+  belongs_to :delivery_day
   validates :category_id,     numericality: { other_than: 1 }
   validates :condition_id,    numericality: { other_than: 1 }
   validates :shipping_fee_id, numericality: { other_than: 1 }
   validates :prefecture_id,   numericality: { other_than: 1 }
+  validates :delivery_day_id, numericality: { other_than: 1 }
 end
