3c8e28201e98bba0e4b1b5b3c9a8411ebd4f214d
kamikazenohiro <kamikazenohiro632@icloud.com>
2021-01-11 18:25:46 +0900

    アクティブハッシュの導入とCategoryクラスの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   belongs_to :user
   
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  validates :genre_id, numericality: { other_than: 1 }
 end
