50331ef5fb8afeab30fc1513d259d934f4c7e6fe
kamikazenohiro <kamikazenohiro632@icloud.com>
2021-01-13 17:37:33 +0900

    不要なテストコードの削除

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,10 +9,6 @@ RSpec.describe Item, type: :model do
       it '各情報が存在すれば登録できる' do
         expect(@item).to be_valid
       end
-      it '価格が300〜9,999,999であれば登録できる' do
-        @item.price = '9999999'
-        expect(@item).to be_valid
-      end
     end
 
     context '商品出品登録がうまくいかないとき' do
