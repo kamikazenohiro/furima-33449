0be8ba0ada88f9cee1ab61c05b4d5b7328b8228e
kamikazenohiro <kamikazenohiro632@icloud.com>
2021-01-13 13:10:24 +0900

    プルダウンのid番号の修正とバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,9 +14,9 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :delivery_day
-  validates :category_id,     numericality: { other_than: 1 }
-  validates :condition_id,    numericality: { other_than: 1 }
-  validates :shipping_fee_id, numericality: { other_than: 1 }
+  validates :category_id,     numericality: { other_than: 0 }
+  validates :condition_id,    numericality: { other_than: 0 }
+  validates :shipping_fee_id, numericality: { other_than: 0 }
   validates :prefecture_id,   numericality: { other_than: 0 }
-  validates :delivery_day_id, numericality: { other_than: 1 }
+  validates :delivery_day_id, numericality: { other_than: 0 }
 end
