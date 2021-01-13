5bc257916457ac3fb51b87c10f234fb20f6ce09b
kamikazenohiro <kamikazenohiro632@icloud.com>
2021-01-13 17:06:51 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,12 @@ class Item < ApplicationRecord
   
   has_one_attached :image
 
-  validates :image,    presence: true
-  validates :name,     presence: true
-  validates :describe, presence: true
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :describe
+  end
+
   validates_numericality_of :price, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999
 
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -14,9 +17,12 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :delivery_day
-  validates :category_id,     numericality: { other_than: 0 }
-  validates :condition_id,    numericality: { other_than: 0 }
-  validates :shipping_fee_id, numericality: { other_than: 0 }
-  validates :prefecture_id,   numericality: { other_than: 0 }
-  validates :delivery_day_id, numericality: { other_than: 0 }
+
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :delivery_day_id
+  end
 end
