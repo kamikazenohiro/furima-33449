3bb59ce6f242ac2d6610225c7f46f3d7020c7d4d
kamikazenohiro <kamikazenohiro632@icloud.com>
2021-01-13 13:03:04 +0900

    javascriptでの販売手数料と販売利益の表示

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,16 @@
-window.addEventListener('load', () => {
-  console.log("OK");
-});
+function item_price (){
+
+  const itemPrice = document.getElementById("item-price");
+  itemPrice.addEventListener('input', function(){
+    const itemPriceValue = itemPrice.value
+    const Tax = 0.1;
+    const tax_fee = itemPriceValue * Tax;
+    const profit = itemPriceValue - tax_fee;
+    const taxArea = document.getElementById("add-tax-price");
+    const profitArea = document.getElementById("profit");
+    taxArea.innerHTML = Math.floor(tax_fee);
+    profitArea.innerHTML = Math.ceil(profit);
+  })
+}
+
+window.addEventListener('load', item_price);
