490aec0e92da8b88695529f2b5a60235ca1d8830
kamikazenohiro <kamikazenohiro632@icloud.com>
2021-01-18 14:26:41 +0900

    データをテーブルに保存する処理

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,7 +10,14 @@ class Order
     validates :block_number
     validates :phone_number
   end
-  
+
   validates :prefecture, numericality: { other_than: 0, message: "Prefecture Select" }
+
+  def save
+    item_buy = ItemBuy.create(user_id: user.id, item_id: item.id) 
+    
+    Address.create(postal_code: postal_code, prefecture: prefecture, municipalities: municipalities, block_number: block_number,
+                   building_name: building_name, phone_number: phone_number, user_id: user.id, item_id: item.id)
+  end
 
 end
