f65c116d24b37ca50351662088acff8a9dc79a41
kamikazenohiro <kamikazenohiro632@icloud.com>
2021-01-18 13:51:21 +0900

    フォルムオブジェクトのバリデーション設定とフォルダ名変更

--- app/models/item_buy.rb
+++
@@ -1,4 +0,0 @@
-class ItemBuy
-  include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :municipalities, :block_number, :building_name, :phone_number, :items_buy
-end
---
+++ app/models/order.rb
@@ -0,0 +1,16 @@
+class Order
+  include ActiveModel::Model
+  attr_accessor: :user, :item, :postal_code, :prefecture, :municipalities, :block_number, :building_name, :phone_number
+
+  with_options presence: true do
+    validates :user
+    validates :item
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :municipalities
+    validates :block_number
+    validates :phone_number
+  end
+  
+  validates :prefecture, numericality: { other_than: 0, message: "Prefecture Select" }
+
+end
