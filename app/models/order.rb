48c3a350b207b1b6c80815b186c77cb132a75e69
kamikazenohiro <kamikazenohiro632@icloud.com>
2021-01-19 21:35:28 +0900

    商品購入ページ電話番号の半角数字のみバリデーション

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,7 +6,7 @@ class Order
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :municipalities
     validates :block_number
-    validates :phone_number
+    validates :phone_number, format: { with: /\A[0-9]+\z/}
   end
 
   validates :prefecture_id, numericality: { other_than: 0, message: "Select" }
