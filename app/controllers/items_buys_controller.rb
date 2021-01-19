c9cd7d4dd7d99a2ba27739865fb9d13bc1e42284
kamikazenohiro <kamikazenohiro632@icloud.com>
2021-01-19 14:57:53 +0900

    購入コントローラと購入画面の処理記述

--- app/controllers/items_buys_controller.rb
+++ app/controllers/items_buys_controller.rb
@@ -5,6 +5,20 @@ class ItemsBuysController < ApplicationController
   end
 
   def create
+    #binding.pry
+    @order = Order.new(items_buy_params)
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def items_buy_params
+    params.require(:order).permit(:postal_code, :prefecture, :municipalities, :block_number, :building_name, :phone_number, :items_buy_id)
   end
 
 end
