1357ea11fa5660baecab3a8892ffdbc6f88c936c
kamikazenohiro <kamikazenohiro632@icloud.com>
2021-01-20 20:48:07 +0900

    商品購入が失敗する時のテストコード

--- spec/factories/items_buys.rb
+++ spec/factories/items_buys.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :order do
+    token          {"tok_abcdefghijk00000000000000000"}
     postal_code    { '111-1111' }
     prefecture_id  { 1 }
     municipalities { 'test' }
     block_number   { 'test' }
     building_name  { 'test' }
-    phone_number   { 1 }
-    token          {"tok_abcdefghijk00000000000000000"}
+    phone_number   { '09012345678' }
   end
 end
--- spec/models/items_buy_spec.rb
+++ spec/models/items_buy_spec.rb
@@ -10,5 +10,48 @@ RSpec.describe Order, type: :model do
         expect(@order).to be_valid
       end
     end
+
+    context '商品購入がうまくいかないとき' do
+      it 'トークンが必須であること' do
+        @order.token = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Token can't be blank")
+      end
+      it '郵便番号が必須であること' do
+        @order.postal_code = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it '郵便番号にハイフンが必須であること' do
+        @order.postal_code = '1111111'
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+      end
+      it '都道府県の選択が必須であること' do
+        @order.prefecture_id = 0
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Prefecture Select")
+      end
+      it '市区町村が必須であること' do
+        @order.municipalities = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Municipalities can't be blank")
+      end
+      it '番地が必須であること' do
+        @order.block_number = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Block number can't be blank")
+      end
+      it '電話番号が必須であること' do
+        @order.phone_number = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it '電話番号にハイフンは不要であること' do
+        @order.phone_number = '0000-00-000'
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone number is invalid")
+      end
+    end
   end
 end
